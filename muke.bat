d06356192da3ae75a4bd8e706595f8f124a83b9c
siterfis <mysiywsite@gmail.com>
2026-06-27 22:21:49 +0900

    Update muke.bat
    making things more simple

--- muke.bat
+++ muke.bat
@@ -1,4 +1,5 @@
 @echo off
+mkdir bin\boot bin\cpu bin\kernel bin\libkern
 i686-elf-gcc -m32 -march=i686 -ffreestanding -nostdlib -O2 -Wall -Wextra -fno-exceptions -std=c23 -I include -MMD -MP -c src\kernel\kernel.c -o bin\kernel\kernel.o
 i686-elf-gcc -m32 -march=i686 -ffreestanding -nostdlib -O2 -Wall -Wextra -fno-exceptions -std=c23 -I include -MMD -MP -c src\kernel\terminal.c -o bin\kernel\terminal.o
 i686-elf-gcc -m32 -march=i686 -ffreestanding -nostdlib -O2 -Wall -Wextra -fno-exceptions -std=c23 -I include -MMD -MP -c src\kernel\panic.c -o bin\kernel\panic.o
@@ -8,6 +9,6 @@ i686-elf-gcc -m32 -march=i686 -ffreestanding -nostdlib -O2 -Wall -Wextra -fno-ex
 i686-elf-gcc -m32 -march=i686 -ffreestanding -nostdlib -O2 -Wall -Wextra -fno-exceptions -std=c23 -I include -MMD -MP -c src\libkern\string.c -o bin\libkern\string.o
 nasm -f elf32 src\boot\boot.asm -o bin\boot\boot.o
 nasm -f elf32 src\cpu\gdt_flush.asm -o bin\cpu\gdt_flush.o
-i686-elf-ld -m elf_i386 -static -T src\linker.ld -o bin\keios.bin bin\boot\boot.o bin\kernel\kernel.o bin\kernel\terminal.o bin\kernel\panic.o bin\cpu\gdt.o bin\libkern\memory.o bin\libkern\stdio.o bin\libkern\string.o
+i686-elf-ld -m elf_i386 -static -T src\linker.ld -o bin\keios.bin bin\boot\boot.o bin\kernel\kernel.o bin\kernel\terminal.o bin\kernel\panic.o bin\cpu\gdt.o bin\libkern\memory.o bin\libkern\stdio.o bin\libkern\string.o bin\cpu\gdt_flush.o
 qemu-system-i386 -kernel bin\keios.bin
 pause
