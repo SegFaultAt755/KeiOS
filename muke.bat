a88c5684b59a7dd17ba7b077a85736acb16a13da
siterfis <mysiywsite@gmail.com>
2026-06-28 23:34:22 +0900

    Update muke.bat
    adding comments

--- muke.bat
+++ muke.bat
@@ -1,11 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
-set "namu=" ::initializing variables
-for /F "usebackq delims=" %%A in (`DIR /B /A:d ".\src"`) do ( ::searching for folders
+::initializing variables
+set "namu=" 
+::searching for folders
+for /F "usebackq delims=" %%A in (`DIR /B /A:d ".\src"`) do ( 
 	if not exist .\bin\%%A (mkdir bin\%%A)
 	echo(
     echo Folder : %%A
-	for %%o in (.\src\%%A\*) do ( ::searching for files
+	::searching for files
+	for %%o in (.\src\%%A\*) do ( 
 		if not "%%~no" == "organize" (
 			if "%%~xo" == ".c" (
 				i686-elf-gcc -m32 -march=i686 -ffreestanding -nostdlib -O2 -Wall -Wextra -fno-exceptions -std=c23 -I include -MMD -MP -c "src\%%A\%%~nxo" -o "bin\%%A\%%~no.o" 
@@ -15,10 +18,12 @@ for /F "usebackq delims=" %%A in (`DIR /B /A:d ".\src"`) do ( ::searching for fo
 				nasm -f elf32 src\%%A\%%~nxo -o bin\%%A\%%~no.o 
 				echo %%~nxo
 			)
-			set "namu=!namu! bin\%%A\%%~no.o " ::storing file path together
+			::storing file path together
+			set "namu=!namu! bin\%%A\%%~no.o " 
 		)
 	)
 )
-i686-elf-ld -m elf_i386 -static -T src\linker.ld -o bin\keios.bin %namu% ::adding the stored file path
+::adding the stored file path
+i686-elf-ld -m elf_i386 -static -T src\linker.ld -o bin\keios.bin %namu% 
 qemu-system-i386 -kernel bin\keios.bin
 pause
