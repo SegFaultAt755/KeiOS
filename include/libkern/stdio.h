bb87ca1c1a927ce5b641fc32492285c61647504b
UserWithARegularName <segfaultat755@gmail.com>
2026-06-29 18:15:01 +0300

    Add outb & inb

--- include/libkern/stdio.h
+++ include/libkern/stdio.h
@@ -19,4 +19,14 @@ void kprint_uint(unsigned int val, int base);
 void kprint_int(int val);
 void kprintf(LogLevel level, const char *fmt, ...);
 
+static inline void outb(uint16_t port, uint8_t value) {
+    __asm__ volatile ("outb %0, %1" : : "a" (value), "Nd" (port));
+}
+
+static inline uint8_t inb(uint16_t port) {
+    uint8_t ret;
+    __asm__ volatile ("inb %1, %0" : "=a" (ret) : "Nd" (port));
+    return ret;
+}
+
 #endif
