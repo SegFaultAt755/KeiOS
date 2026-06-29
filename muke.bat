69e8c861fc28134458c284d7b5d7d6092b956fe0
siterfis <mysiywsite@gmail.com>
2026-06-29 21:58:15 +0900

    Update muke.bat
    more deeper

--- muke.bat
+++ muke.bat
@@ -7,22 +7,27 @@ for /F "usebackq delims=" %%A in (`DIR /B /A:d ".\src"`) do (
 	if not exist .\bin\%%A (mkdir bin\%%A)
 	echo(
     echo Folder : %%A
-	::searching for files
-	for %%o in (.\src\%%A\*) do ( 
-		if not "%%~no" == "organize" (
-			if "%%~xo" == ".c" (
-				i686-elf-gcc -m32 -march=i686 -ffreestanding -nostdlib -O2 -Wall -Wextra -fno-exceptions -std=c23 -I include -MMD -MP -c "src\%%A\%%~nxo" -o "bin\%%A\%%~no.o" 
-				echo %%~nxo
+		for /F "usebackq delims=" %%A in (`DIR /B /A:d ".\src"`) do ( 
+		if not exist .\bin\%%A (mkdir bin\%%A)
+		echo(
+		echo Folder : %%A
+		::searching for files
+			for %%o in (.\src\%%A\*) do ( 
+				if not "%%~no" == "organize" (
+					if "%%~xo" == ".c" (
+						i686-elf-gcc -m32 -march=i686 -ffreestanding -nostdlib -O2 -Wall -Wextra -fno-exceptions -std=c23 -I include -MMD -MP -c "src\%%A\%%~nxo" -o "bin\%%A\%%~no.o" 
+						echo %%~nxo
+					)
+					if "%%~xo" == ".asm" (
+						nasm -f elf32 src\%%A\%%~nxo -o bin\%%A\%%~no.o 
+						echo %%~nxo
+					)
+					::storing file path together
+					set "namu=!namu! bin\%%A\%%~no.o " 
+				)
 			)
-			if "%%~xo" == ".asm" (
-				nasm -f elf32 src\%%A\%%~nxo -o bin\%%A\%%~no.o 
-				echo %%~nxo
-			)
-			::storing file path together
-			set "namu=!namu! bin\%%A\%%~no.o " 
 		)
 	)
-)
 ::adding the stored file path
 i686-elf-ld -m elf_i386 -static -T src\linker.ld -o bin\keios.bin %namu% 
 qemu-system-i386 -kernel bin\keios.bin
