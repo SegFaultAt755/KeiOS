2c922f008aec4334e0e15bca73cc02fa22ec9b53
siterfis <mysiywsite@gmail.com>
2026-07-01 02:04:28 +0900

    Update muke.bat

--- muke.bat
+++ muke.bat
@@ -6,6 +6,7 @@ setlocal enabledelayedexpansion
 REM initializing variables
 set "namu=" 
 REM searching for folders
+echo Compiling
 for /f "tokens=*" %%f in ('dir /b /ad /s "%StartPath%"') do ( 
     set "FullPath=%%f"
     set "RelativePath=!FullPath:%StartPath%\=!"
@@ -27,9 +28,19 @@ for /f "tokens=*" %%f in ('dir /b /ad /s "%StartPath%"') do (
 		)
 	)
 )
-echo Compiling
-REM adding the stored file path
-i686-elf-ld -m elf_i386 -static -T src\linker.ld -o bin\keios.bin %namu% 
-qemu-system-i386 -kernel bin\keios.bin
+cls
+echo "Linking & Copying"
+REM adding the stored file path & creating
+mkdir isomer isomer\boot isomer\boot\grub 
+copy menu.lst isomer\boot\grub
+copy stage1 isomer\boot\grub
+copy stage2 isomer\boot\grub
+copy data_file isomer\boot
+i686-elf-ld -m elf_i386 -static -T src\linker.ld -o isomer/boot/keios.bin %namu% 
+
+REM making the iso
+xorriso -outdev keios.iso -blank as_needed -map isomer / -boot_image grub bin_path=/boot/grub/stage1
+rmdir /s /q isomer bin
+qemu-system-i386 -cdrom keios.iso
 endlocal
 pause
