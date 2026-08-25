647f3cd4a71856634be38c67ffaf00387aae4eca
UserWithARegularName <segfaultat755@gmail.com>
2026-08-25 12:36:24 +0300

    I see

--- src/kernel/acpi.c
+++ src/kernel/acpi.c
@@ -48,18 +48,17 @@ void acpi_init(struct rsdp *rsdp) {
 
     if (is_xsdt) {
         struct rsdp20 *rsdp20 = (struct rsdp20 *)rsdp;
-        root_phys_addr = ADD_KERNEL_OFFSET((uintptr_t)rsdp20->xsdt_address);
+        root_phys_addr = (uintptr_t)rsdp20->xsdt_address;
     } else {
-        root_phys_addr = ADD_KERNEL_OFFSET((uintptr_t)rsdp->rsdt_address);
+        root_phys_addr = (uintptr_t)rsdp->rsdt_address;
     }
 
     /* Reserve physical frame and map physical address into kernel virtual memory */
     pmm_reserve_phys(root_phys_addr, sizeof(struct acpi_header));
-    vmm_map_page((uint32_t)root_phys_addr, (uint32_t)ADD_KERNEL_OFFSET(root_phys_addr), PTE_PRESENT | PTE_RW);
+    vmm_map_page((uint32_t)ADD_KERNEL_OFFSET(root_phys_addr), (uint32_t)root_phys_addr, PTE_PRESENT | PTE_RW | PDE_PWT);
 
     /* Map the ACPI root table into virtual memory */
-    struct acpi_header *root_table = (struct acpi_header *)root_phys_addr;
-
+    struct acpi_header *root_table = (struct acpi_header *)ADD_KERNEL_OFFSET(root_phys_addr);
     struct acpi_header *fadt_header = find_fadt(root_table, is_xsdt);
     if (!fadt_header)
         KERNEL_PANIC("Failed to find FACP signature in root table", "Invalid root table from RSPD");
